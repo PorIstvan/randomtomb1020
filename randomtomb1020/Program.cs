b49352d9289ef8c3f1339904221fdf91490eb300
István Pór <por.istvan@verebelyszki.hu>
2020-10-20 12:08:06 +0200

    feladat

---
+++ randomtomb1020/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace randomtomb1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] tb = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < tb.Length; i++)
+            {
+                tb[i] = rnd.Next(10, 100);
+
+            }
+            Console.WriteLine("A tomb elemei:");
+            for (int i = 0; i < tb.Length; i++)
+            {
+                Console.WriteLine(tb[i]);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
